62dc3c9abec6498fb861a9dd65e99cb9c2d148c1
plexity <plexthesavior@gmail.com>
2022-07-18 22:57:11 -0500

    Co-authored-by: Daniel Van <Daniel-Van@users.noreply.github.com>

--- tile.h
+++ tile.h
@@ -1,23 +1,30 @@
-#include <string>
 #include <iostream>
 #include "board.h"
 #include <vector>
 
-#ifndef __TILE_H__
-#define __TILE_H__
+#ifndef __TILE_H_
+#define __TILE_H_
+class Player;
+class Board;
+class Tile
+{
 
-class tile : Board {
-    private:
-        std::string blockName;
-        std::string monopolyType;
-        int purchaseCost;
-        int improvementCost;
-        std::vector<int> tuitionWithExpenses;
-        
-    public:
-        tile(std::string blockName, std::string monopolyType, int purchaseCost, int imporvementCost,
-           std::vector <int> tuitionWithExpenses);
-        ~tile();
+public:
+    // Tile(std::string name, int pos);
+    Tile();
+    virtual ~Tile();
+    virtual Board *getBoard();
+    virtual Player *getOwner() = 0;
+    virtual void setOwner(Player *player) = 0;
+    virtual int getImprovement() = 0;
+    virtual void setImprovement(int x) = 0;
+    virtual void action(Player *player) = 0;
+    virtual void mortgage(Player *player) = 0;
+    virtual void auction() = 0;
+    virtual bool isMortgaged() = 0;
+    virtual bool isOwnable() = 0;
+    virtual int getPos() = 0;
+    virtual int getPrice() = 0;
 };
 
 #endif
